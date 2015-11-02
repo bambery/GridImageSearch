6ef95cb8a336fdd57eaaebeb531115e0be7aa99b
Lauren Wszolek <lwszolek@groupon.com>
2015-11-01 23:26:59 -0800

    almost to partially sort of using a recycler view

--- adapters/ImageResultAdapter.java
+++ adapters/ImageResultAdapter.java
@@ -1,24 +1,65 @@
 package wszolek.lauren.imagesearch.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import wszolek.lauren.imagesearch.R;
+import wszolek.lauren.imagesearch.holders.ImageHolder;
 import wszolek.lauren.imagesearch.models.ImageResult;
 
-public class ImageResultAdapter extends ArrayAdapter<ImageResult> {
-    private static class ViewHolder {
-        ImageView ivImage;
+public class ImageResultAdapter extends RecyclerView.Adapter<ImageHolder> {
+    private ArrayList<ImageResult> imageResults;
+    private Context context;
+
+    public ImageResultAdapter(Context context, ArrayList<ImageResult> imageResults) {
+        this.imageResults = imageResults;
+        this.context = context;
     }
 
+    @Override
+    public ImageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_image_result, null);
+        ImageHolder imageHolder = new ImageHolder(layoutView);
+        return imageHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ImageHolder imageHolder, int position) {
+        ImageResult imageResult = imageResults.get(position);
+        Picasso.with(context)
+                .load(imageResult.thumbUrl)
+// implement this in a min
+//                .placeholder(R.drawable.placeholder)
+                .into(imageHolder.ivImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.imageResults.size();
+    }
+
+}
+/*
+
+
+
+        public void bindImageResult(ImageResult imageResult) {
+            mImageResult = imageResult;
+            Picasso.with(getContext())
+                    .load(imageResult.thumbUrl)
+// implement this in a min for the color placeholders
+//                .placeholder(R.drawable.placeholder)
+                    .into(ivImage);
+
+        }
+    }
     public ImageResultAdapter(Context context, ArrayList<ImageResult> images) {
         super(context, R.layout.item_image_result, images);
     }
@@ -47,3 +88,4 @@ public class ImageResultAdapter extends ArrayAdapter<ImageResult> {
         return convertView;
     }
 }
+*/
